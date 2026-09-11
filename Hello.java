431bf97d0c0bbf1896a93a18c2b23dee9b9c487a
Harikiran05 <harikirandowpati@gmail.com>
2026-09-11 17:19:59 +0530

    Learning Java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+  public static void main(String[] args) {
+    int number = 5;
+    byte b = 10;
+    short s = 20;
+    long l = 30L;
+
+    float f = 40.0f;
+    double d = 50.0;
+
+    char c = 'A';
+
+    boolean bool = true;
+    
+  }
+}

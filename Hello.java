04d01e6caf3c74e56cbacc67d9c5842100c98410
Harikiran05 <harikirandowpati@gmail.com>
2026-09-14 16:07:23 +0530

    Practice

--- Hello.java
+++ Hello.java
@@ -1,18 +1,14 @@
 public class Hello {
   public static void main(String[] args) {
-    int a = 10;
-    int b = 20;
-    int c = 30;
-    int d = 40;
-    if (a > b) {
-      System.out.println("a is greater than b");
-    } else if (b > c) {
-      System.out.println("b is greater than c");
-    } else if (c > d) {
-      System.out.println("c is greater than d");
-    } else {
-      System.out.println("d is the greatest");
-      
+    int i = 1;
+    while (i <= 4) {
+      System.out.println("Hello, World!");
+      int j = 1;
+      while (j <= 3) {
+        System.out.println("Inner loop: " + j);
+        j++;
+      }
+      i++;
     }
-  }
+}
 }

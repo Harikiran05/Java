068b36a991fadddef6776eae13cf3b175f92bea8
Harikiran05 <harikirandowpati@gmail.com>
2026-09-14 20:27:40 +0530

    Practice

--- Hello.java
+++ Hello.java
@@ -1,14 +1,15 @@
+class Calculator {
+  public int  add(int a , int b) {
+    return a + b;
+  }
+}
+
 public class Hello {
   public static void main(String[] args) {
-    int i = 1;
-    while (i <= 4) {
-      System.out.println("Hello, World!");
-      int j = 1;
-      while (j <= 3) {
-        System.out.println("Inner loop: " + j);
-        j++;
-      }
-      i++;
-    }
+    Calculator calc = new Calculator();
+    int result = calc.add(5, 3);
+    System.out.println("Result: " + result);
+  }
 }
-}
+
+

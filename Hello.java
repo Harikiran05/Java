0644be46d44de65653d0d064a159fe3fde6570eb
Harikiran05 <harikirandowpati@gmail.com>
2026-09-15 19:40:31 +0530

    Practice

--- Hello.java
+++ Hello.java
@@ -2,11 +2,13 @@
 
 public class Hello {
   public static void main(String[] args) {
-    int nums[][] = new int[3][4];
+    int nums[][][] = new int[3][4][5];
 
     for(int i = 0; i < 3; i++) {
       for(int j = 0; j < 4; j++) {
-        nums[i][j] = (int)(Math.random() * 10);
+        for(int k = 0; k < 5; k++) {
+          nums[i][j][k] = (int)(Math.random() * 10);
+        }
       }
     }
 
@@ -17,9 +19,12 @@ public class Hello {
     //   System.out.println();
     // }
 
-    for(int n[] : nums) {
-      for(int x : n) {
-        System.out.print(x + " ");
+    for(int n[][] : nums) {
+      for(int x[] : n) {
+        for(int y : x) {
+          System.out.print(y + " ");
+        }
+        System.out.println();
       }
       System.out.println();
     }
